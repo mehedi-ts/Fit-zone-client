07be23764fa17ee6613715716e373db98e1b9a19
mehedi hasan <mehedihasan.j135@gmail.com>
2026-06-29 13:32:35 +0600

    feat: enhance getClassById function with authorization header and error handling

--- src/app/lib/api/getClassById.js
+++ src/app/lib/api/getClassById.js
@@ -1,8 +1,20 @@
 import { getTokenServer } from "../getTokenServer";
 
 const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
+
 export async function getClassById(classId) {
   const token = await getTokenServer();
-  const res = await fetch(`${SERVER_URL}/api/classes/${classId}`);
+
+  const res = await fetch(`${SERVER_URL}/api/classes/${classId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error(`Failed to fetch class: ${res.status}`);
+  }
+
   return res.json();
 }
