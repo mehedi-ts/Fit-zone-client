fed0262e1489efc8cb3b6f68b464ff6ecf2ad803
mehedi hasan <mehedihasan.j135@gmail.com>
2026-06-29 21:23:14 +0600

    feat: update getAllForums function to include cache control and improve error handling

--- src/app/lib/api/getAllForum.js
+++ src/app/lib/api/getAllForum.js
@@ -1,12 +1,16 @@
-import { getTokenServer } from "../getTokenServer";
-
 const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
 
 export async function getAllForums() {
   try {
-    const res = await fetch(`${SERVER_URL}/api/forums`);
+    const res = await fetch(`${SERVER_URL}/api/forums`, {
+      cache: "no-store",
+    });
 
-    return res.json();
+    if (!res.ok) {
+      throw new Error("Failed to fetch forums");
+    }
+
+    return await res.json();
   } catch (error) {
     console.error("Error:", error);
     throw error;
